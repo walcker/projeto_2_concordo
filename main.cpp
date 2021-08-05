3d47caeb84c6dcb38daa28d87a5753054bedf5c9
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-08-05 23:45:28 +0000

    Teste de case

--- main.cpp
+++ main.cpp
@@ -9,91 +9,106 @@
 
 using namespace std;
 
+void limpaTela(){
+  system("clear");
+}
+
 int main(){
 
+  //Variáveis
+  int funcaoDesejada = 1;
   string teste;
   vector<string> mensagens;
 
-	Usuario u;
-	u.setId(1);
-	u.setEmail("walcker@ufrn.br");
-  u.setSenha("1234");
-  u.setNome("Walcker Gomes");
+while(funcaoDesejada < 10 && funcaoDesejada > 0){
 
-	cout << "E-mail = " << u.getEmail() << ", Nome = " << u.getNome() << endl;
+        //Mostrando o menu
+        cout << "Operações" << endl;
+        cout << "1 - Login." << endl;
+        cout << "2 - Cadastrar novo usuário." << endl;
+        cout << "3 - Escrever uma mensagem." << endl;
+        cout << "4 - Criar canal." << endl;
+        cout << "5 - Criar servidor." << endl;
+        cout << "9 - Sair do sistema." << endl;
+        cout << "Escolha um número e pressione ENTER." << endl;
 
-  cout << endl;
+        //Lendo opção do usuário
+        cin >> funcaoDesejada;
 
-  Mensagem m;
-	m.setDataHora("02/12/1979 08:00");
-	m.setEnviadaPor("Walcker Gomes");
-  m.setConteudo("Bom dia.");
+        limpaTela();
 
-	cout << "Data/Hora = " << m.getDataHora() <<", Nome = " << m.getEnviadaPor() << ", Mensagem: " << m.getConteudo() << endl;
+        //Lendo a função desejada
+        switch(funcaoDesejada){
+            case 1:
+                cout << "Função escolhida 1" << endl;
+                
+                break;
+            case 2:
+                cout << "Função escolhida 2" << endl;
+                Usuario u;
+                u.setId(1);
+                u.setEmail("walcker@ufrn.br");
+                u.setSenha("1234");
+                u.setNome("Walcker Gomes");
 
-  cout << endl;
+                cout << "E-mail = " << u.getEmail() << ", Nome = " << u.getNome() << endl;
+                break;
+            /*case 3:
+                cout << "Escreva uma Mensagem" << endl;
+                Mensagem m;
+                m.setDataHora("02/12/1979 08:00");
+                m.setEnviadaPor("Walcker Gomes");
+                m.setConteudo("Bom dia.");
 
-  Canaltexto c;
-	c.nome = ("Nome do canal");
-  
-	for(int i = 0; i < 5; i++){
-    cout << "Digite" << endl;
-    cin >> teste;
-      c.mensagens.push_back(teste);
+                cout << "Data/Hora = " << m.getDataHora() <<", Nome = " << m.getEnviadaPor() << ", Mensagem: " << m.getConteudo() << endl;
+                break;
+            case 4:
+                cout << "Criando um canal." << endl;
+                Canaltexto c;
+                c.nome = ("Nome do canal");
+                
+                for(int i = 0; i < 5; i++){
+                  cout << "Digite" << endl;
+                  cin >> teste;
+                    c.mensagens.push_back(teste);
+                  }
+
+
+
+                cout << "Mensagens do canal: = " << c.Nome() << endl;
+                for(int i = 0; i< 5; i++){
+                    cout << c.mensagens[i] << " ";
+                  }
+                break;
+            case 5:
+                cout << "Criando Servidor" << endl;
+                Sistema si;
+                for(int i = 0; i < 5; i++){
+                  cout << "Usuários" << endl;
+                  cin >> teste;
+                    si.usuarios.push_back(teste);
+                  }
+                for(int i = 0; i < 5; i++){
+                  cout << "Servidores" << endl;
+                  cin >> teste;
+                    si.servidores.push_back(teste);
+                  }
+
+                for(int i = 0; i< 5; i++){
+                    cout << si.usuarios[i] << " ";
+                  }
+                  for(int i = 0; i< 5; i++){
+                    cout << si.servidores[i] << " ";
+                  }
+                break;
+            case 9:
+                cout << "saindo do sistema" << endl;
+                break;*/
+        }
+
     }
 
-
-
-	cout << "Mensagens do canal: = " << c.Nome() << endl;
-  for(int i = 0; i< 5; i++){
-      cout << c.mensagens[i] << " ";
-    }
-
-  cout << endl;
-
-  Servidor s;
-	s.id = (01);
-	s.nome = ("Walcker Gomes");
-  s.descricao = ("Servidor Geral");
-  s.convite = ("Link para Convite");
-  for(int i = 0; i < 5; i++){
-    cout << "Digite" << endl;
-    cin >> teste;
-      s.canais.push_back(teste);
-    }
-  for(int i = 0; i < 5; i++){
-    s.ids.push_back(i);
-    }
-
-
-	cout << "Nome Usuário = " << s.Nome() << ", Servidor = " << s.Descricao() << " Link de convite = " << s.Convite() << " Canais no servidor: " << endl;
-  for(int i = 0; i< 5; i++){
-      cout << s.canais[i] << " ";
-    }
-  for(int i = 0; i< 5; i++){
-      cout << s.ids[i] << " ";
-    }
-
-  cout << endl;
-
-  Sistema si;
-	for(int i = 0; i < 5; i++){
-    cout << "Usuários" << endl;
-    cin >> teste;
-      si.usuarios.push_back(teste);
-    }
-  for(int i = 0; i < 5; i++){
-    cout << "Servidores" << endl;
-    cin >> teste;
-      si.servidores.push_back(teste);
-    }
-
-  for(int i = 0; i< 5; i++){
-      cout << si.usuarios[i] << " ";
-    }
-    for(int i = 0; i< 5; i++){
-      cout << si.servidores[i] << " ";
-    }
+    system("pause"); 
 
 	return 0;
 }
